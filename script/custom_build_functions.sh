d17b3556697384c08e569bdc613755ac58a28bd8
Phil Pirozhkov <hello@fili.pp.ru>
2020-11-27 00:03:22 +0300

    EXPERIMENT: Run cucumber on JRuby
    In hopes it doesn't take 45+ minutes anymore

--- script/custom_build_functions.sh
+++ script/custom_build_functions.sh
@@ -2,16 +2,10 @@ function run_cukes {
   if [ -d features ]; then
     echo "${PWD}/bin/cucumber"
 
-    if is_jruby; then
-      echo "WARNING: Cucumber is skipped on JRuby on rspec-core due to" \
-           "excessive build times (>45 minutes) causing timeouts on Travis"
-      return 0
-    else
-      # Prepare RUBYOPT for scenarios that are shelling out to ruby,
-      # and PATH for those that are using `rspec` or `rake`.
-      RUBYOPT="${RUBYOPT} -I${PWD}/../bundle -rbundler/setup" \
-         PATH="${PWD}/bin:$PATH" \
-         bin/cucumber --strict
-    fi
+    # Prepare RUBYOPT for scenarios that are shelling out to ruby,
+    # and PATH for those that are using `rspec` or `rake`.
+    RUBYOPT="${RUBYOPT} -I${PWD}/../bundle -rbundler/setup" \
+       PATH="${PWD}/bin:$PATH" \
+       bin/cucumber --strict
   fi
 }
